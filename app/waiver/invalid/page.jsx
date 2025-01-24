db4fa665f4243b7a007ced5972c4ac0aac6aaaeb
taltmann0818 <42226531+taltmann0818@users.noreply.github.com>
2025-01-24 16:46:39 -0500

    Styling updates

--- app/waiver/invalid/page.jsx
+++ app/waiver/invalid/page.jsx
@@ -3,11 +3,9 @@ import React from 'react';
 
 const InvalidPage = () => {
   return (
-    <div style={{ margin: '0 auto' }}>
       <div class="message-container" style={{ backgroundColor: '#ffabab' }}>
         <h1>Not Validated!</h1>
       </div>
-    </div>
   );
 };
 
--- app/waiver/valid/page.jsx
+++ app/waiver/valid/page.jsx
@@ -3,11 +3,9 @@ import React from 'react';
 
 const ValidPage = () => {
   return (
-    <div style={{ margin: '0 auto' }}>
       <div class="message-container" style={{ backgroundColor: '#4CAF50' }}>
         <h1>Validated</h1>
       </div>
-    </div>
   );
 };
 
