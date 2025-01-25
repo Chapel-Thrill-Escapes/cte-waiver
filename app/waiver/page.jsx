df64450dc40ffd8d8d9a56db5f12e036d09d00dd
taltmann0818 <42226531+taltmann0818@users.noreply.github.com>
2025-01-24 22:37:30 -0500

    test updates

--- app/waiver/page.jsx
+++ app/waiver/page.jsx
@@ -2,29 +2,27 @@
 import React, { useState } from 'react';
 import { QrReader } from 'react-qr-reader';
 
-function
- Scan() {
+const Home = (props) => {
     const [data, setData] = useState('No result');
 
     return (
-        <>
+    <>
         <QrReader
             onResult={(result, error) => {
-            if (!!result) {
-                setData(result?.text);
-            }
+                if (!!result) {
+                    setData(result?.text);
+                }
 
-            if (!!error) {
-                console.info(error);
-            }
+                if (!!error) {
+                    console.info(error);
+                }
             }}
             constraints={{ facingMode: 'environment' }}
             style={{ width: '40%', height: '40%' }}
             legacyMode="true"
         />
         <p>{data}</p>
-        </>
+    </>
     );
 };
-
-export default Scan;
+export default Home;
